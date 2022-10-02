90b2571e8b4cae580ec9a17be282b5a079a52d21
RamilBinnatov <ramilrb@code.edu.az>
2022-10-03 01:56:34 +0400

    ASP

---
+++ Asp-intro/Asp-intro/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp_intro.Controllers
+{
+    public class ContactController
+    {
+        public string Index()
+        {
+            return "Contact Page";
+        }
+
+      
+    }
+}
